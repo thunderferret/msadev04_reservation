f7af75e9517cd6f023a93c849609c0d61e0b4cf2
seo <vpflt992@gmail.com>
2021-06-08 16:53:17 +0900

    kafka config local template updated

--- src/main/java/global/bizdevelope/realmanapp/ReservationApplication.java
+++ src/main/java/global/bizdevelope/realmanapp/ReservationApplication.java
@@ -7,11 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
-
 
 @SpringBootApplication
 public class ReservationApplication {
--- src/test/java/global/bizdevelope/realmanapp/ReservationApplicationTests.java
+++ src/test/java/global/bizdevelope/realmanapp/ReservationApplicationTests.java
@@ -1,8 +1,12 @@
 package global.bizdevelope.realmanapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+@EnableAutoConfiguration(exclude= KafkaAutoConfiguration.class)
 @SpringBootTest
 class ReservationApplicationTests {
 
