b07b78e11bcd5df7dc324d2d4bb1ec74a3ac1143
seo <vpflt992@gmail.com>
2021-06-09 09:48:50 +0900

    kubectl setting for eks

--- src/main/java/global/bizdevelope/realmanapp/ReservationApplication.java
+++ src/main/java/global/bizdevelope/realmanapp/ReservationApplication.java
@@ -2,13 +2,10 @@ package global.bizdevelope.realmanapp;
 
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class ReservationApplication {
